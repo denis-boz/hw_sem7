1bd4057d110fd0bc9f560b47c67f8906c7d9dbba
Denis <denis-boz@hotmail.com>
2023-06-26 10:20:11 +0300

    Решение задачи 50

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
+/*
 Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -41,3 +42,61 @@ void PrintMatrix(double[,] array)
         Console.WriteLine();
     }
 }
+*/
+
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultMatrix = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(resultMatrix);
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        
+            matrix[i,j] = new Random().Next(min, max +1);
+    
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите координаты");
+ int m = Convert.ToInt32(Console.ReadLine());
+ int n = Convert.ToInt32(Console.ReadLine());
+ if (m>rows || n>columns)
+ Console.WriteLine("такого элемента нет");
+ else
+ {
+ object c = resultMatrix.GetValue(m,n)!;
+ Console.WriteLine(c);
+ }
