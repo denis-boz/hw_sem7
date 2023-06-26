d90803ab7471caa3f186ee40f9835d21f0f40025
Denis <denis-boz@hotmail.com>
2023-06-26 11:49:08 +0300

    Решение задачи 52

--- Program.cs
+++ Program.cs
@@ -55,6 +55,7 @@ void PrintMatrix(double[,] array)
 17 -> такого числа в массиве нет
 */
 
+/*
 Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -100,3 +101,65 @@ Console.WriteLine("Введите координаты");
  object c = resultMatrix.GetValue(m,n)!;
  Console.WriteLine(c);
  }
+ */
+
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultMatrix = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(resultMatrix);
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = new Random().Next(min, max + 1);
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            sum = sum + matr[j, i];
+        }
+        Console.WriteLine($"Среднее арифметичекое столбца {i + 1} = {sum / matr.GetLength(0)}");
+    }
+}
+Console.WriteLine();
+GetAverage(resultMatrix);
